03205801626ba2ba9266adbe765a0d74388cb399
John Smith <john@john-pc.john-pc>
2018-04-22 21:53:43 +1000

    Refactor and tests for the generation of RGB categories

--- application/rgb.py
+++ application/rgb.py
@@ -1,60 +1,36 @@
-def get_colour_categories(number_of_categories):
-    if number_of_categories == 2:
-        colour_categories = [
-            {
-                'min' : 0,
-                'max' : 127,
-                'median' : 64
-            },
-            {
-                'min' : 128,
-                'max' : 255,
-                'median' : 192
-            }
-        ]
-    elif number_of_categories == 3:
-        colour_categories = [
-            {
-                'min' : 0,
-                'max' : 84,
-                'median' : 42
-            },
-            {
-                'min' : 85,
-                'max' : 170,
-                'median' : 127
-            },
-            {
-                'min' : 171,
-                'max' : 255,
-                'median' : 213
-            }
-        ]
-    elif number_of_categories == 4:
-        colour_categories = [
-            {
-                'min' : 0,
-                'max' : 63,
-                'median' : 32
-            },
-            {
-                'min' : 64,
-                'max' : 127,
-                'median' : 96
-            },
-            {
-                'min' : 128,
-                'max' : 191,
-                'median' : 150
-            },
-            {
-                'min' : 192,
-                'max' : 255,
-                'median' : 222
-            }
-        ]
+import math
 
-    return colour_categories 
+def update_category_max(category_max, category_min, category_size):
+    return category_min + category_size
+
+def get_size_of_categories(number_of_categories):
+    return math.floor(255 / number_of_categories)
+
+def get_category_median(category_min, category_size):
+    return category_min + math.ceil(category_size / 2)
+
+def create_category(category_max, category_min, category_size):
+    category = {
+        'min' : category_min,
+        'max' : category_max,
+        'median' : get_category_median(category_min, category_size)
+    }
+    return category
+
+def update_category_min(category_max, category_min):
+    return category_max + 1
+
+def get_colour_categories(number_of_categories):
+    colour_categories = []
+    category_size = get_size_of_categories(number_of_categories)
+    category_min = 0
+    category_max = 0
+    for _ in range(number_of_categories):
+        category_max = update_category_max(category_max, category_min, category_size)
+        category = create_category(category_max, category_min, category_size)
+        colour_categories.append(category)
+        category_min = update_category_min(category_max, category_min)
+    return colour_categories
 
 def get_categories(red_category_number, green_category_number, blue_category_number):
     min_limit = 0
---
+++ tests/test_rgb.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import pytest
+import application.rgb
+
+def test_get_colour_categories_2():
+    colour_categories = application.rgb.get_colour_categories(2)
+
+    assert colour_categories[0]['min'] == 0
+    assert colour_categories[0]['max'] == 127
+    assert colour_categories[0]['median'] == 64
+
+    assert colour_categories[1]['min'] == 128
+    assert colour_categories[1]['max'] == 255
+    assert colour_categories[1]['median'] == 192 
+
+def test_get_colour_categories_4():
+    colour_categories = application.rgb.get_colour_categories(4)
+
+    assert colour_categories[0]['min'] == 0
+    assert colour_categories[0]['max'] == 63
+    assert colour_categories[0]['median'] == 32
+
+    assert colour_categories[1]['min'] == 64 
+    assert colour_categories[1]['max'] == 127
+    assert colour_categories[1]['median'] == 96
+
+    assert colour_categories[2]['min'] == 128 
+    assert colour_categories[2]['max'] == 191
+    assert colour_categories[2]['median'] == 160
+
+    assert colour_categories[3]['min'] == 192
+    assert colour_categories[3]['max'] == 255
+    assert colour_categories[3]['median'] == 224
+
